de93faf0ab22c2d84d3be5822ce44e3d91207ba0
ZeharaY <106754679+zeharaY@users.noreply.github.com>
2023-02-23 10:32:00 +0300

    com
    com

--- web_flask/0-hbnb.py
+++ web_flask/0-hbnb.py
@@ -33,6 +33,7 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = uuid.uuid4()
 
     return render_template('100-hbnb.html',
                            states=st_ct,
